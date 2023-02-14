c1637afb2fbb1c513e8e3c73d9db667aaa50aa2d
pietro <s314518@studenti.polito.it>
2023-02-14 10:51:57 +0100

    Creato header "Hitbox.h"

--- SimpleOpenGLTexturedLoader/SimpleOpenGLTexturedLoader/PlayingState.cpp
+++ SimpleOpenGLTexturedLoader/SimpleOpenGLTexturedLoader/PlayingState.cpp
@@ -13,6 +13,7 @@
 #include "Const.h"
 #include "Context.h"
 #include "Const.h"
+#include "Hitbox.h"
 #include <vector>
 #define _USE_MATH_DEFINES
 #include <math.h>
@@ -248,97 +249,6 @@ void PlayingState::handleInputUp(unsigned char key, int x, int y) {
 
 void PlayingState::checkCollisions() {
 	//check all relevant collisions between objects in current level e. g. player and ground, player and collectible items
-
-
-	struct HitboxVertex {
-		//defines a vertex
-		HitboxVertex(int x, int y, int z) : x(x), y(y), z(z) {};
-		HitboxVertex() : x(0), y(0), z(0) {};
-
-		int x;
-		int y;
-		int z;
-	};
-
-	struct HitboxFace {
-		//defines a hitbox face (separating a bounding box into its 6 faces may be useful to solve collisions with respecto to the direction of the collision)
-		HitboxFace(HitboxVertex p1, HitboxVertex p2, HitboxVertex p3, HitboxVertex p4) : p1(p1), p2(p2), p3(p3), p4(p4), vertices() {
-			vertices[0] = p1;
-			vertices[1] = p2;
-			vertices[2] = p3;
-			vertices[3] = p4;
-		};
-
-		HitboxFace() {}
-
-		HitboxVertex p1;
-		HitboxVertex p2;
-		HitboxVertex p3;
-		HitboxVertex p4;
-		HitboxVertex vertices[4];
-	};
-
-	struct Hitbox {
-		//defines a hitbox
-		Hitbox(aiVector3D min, aiVector3D max, HitboxFace left, HitboxFace right, HitboxFace top, HitboxFace bottom, HitboxFace front, HitboxFace back) :
-			min(min), max(max), left(left), right(right), top(top), bottom(bottom), front(front), back(back) {};
-		Hitbox(aiVector3D min, aiVector3D max) : min(min), max(max) {
-
-			left = HitboxFace(HitboxVertex(min.x, min.y, min.z),
-				HitboxVertex(min.x, min.y, max.z),
-				HitboxVertex(min.x, max.y, max.z),
-				HitboxVertex(min.x, max.y, min.z));
-
-			right = HitboxFace(HitboxVertex(max.x, min.y, min.z),
-				HitboxVertex(max.x, min.y, max.z),
-				HitboxVertex(max.x, max.y, max.z),
-				HitboxVertex(max.x, max.y, min.z));
-
-			bottom = HitboxFace(HitboxVertex(min.x, min.y, min.z),
-				HitboxVertex(max.x, min.y, min.z),
-				HitboxVertex(max.x, min.y, max.z),
-				HitboxVertex(min.x, min.y, max.z));
-
-			top = HitboxFace(HitboxVertex(min.x, max.y, min.z),
-				HitboxVertex(max.x, max.y, min.z),
-				HitboxVertex(max.x, max.y, max.z),
-				HitboxVertex(min.x, max.y, max.z));
-
-			back = HitboxFace(HitboxVertex(min.x, min.y, min.z),
-				HitboxVertex(max.x, min.y, min.z),
-				HitboxVertex(max.x, max.y, min.z),
-				HitboxVertex(min.x, max.y, min.z));
-
-			front = HitboxFace(HitboxVertex(min.x, min.y, max.z),
-				HitboxVertex(max.x, min.y, max.z),
-				HitboxVertex(max.x, max.y, max.z),
-				HitboxVertex(min.x, max.y, max.z));
-		}
-
-		aiVector3D min;
-		aiVector3D max;
-
-		HitboxFace left;
-		HitboxFace right;
-		HitboxFace top;
-		HitboxFace bottom;
-		HitboxFace front;
-		HitboxFace back;
-	};
-
-	auto bboxIntersection = [](Hitbox a, Hitbox b) {
-		//return true if a and b intersect
-		bool result = (
-			a.min.x < b.max.x&&
-			a.max.x > b.min.x &&
-			a.min.y < b.max.y&&
-			a.max.y > b.min.y &&
-			a.min.z < b.max.z&&
-			a.max.z > b.min.z
-			);
-
-		return result;
-	};
 
 	aiVector3D* playerMin = new aiVector3D(0, 0, 0);
 	aiVector3D* playerMax = new aiVector3D(0, 0, 0);
